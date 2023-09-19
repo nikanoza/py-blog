79833c6b9b465796d26deaffb289c1ea61db0931
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-19 12:05:26 +0400

    feat: create post model

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
