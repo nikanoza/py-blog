7d88d7f5f5ca98149091428c99d1cfb3bddfc36e
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-19 12:34:54 +0400

    create views functions

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Post, Category, Author
+from .serializers import PostSerializer, CategorySerializer, AuthorSerializer
 
-# Create your views here.
+class PostListView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class PostsByCategoryView(ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
+        return Post.objects.filter(category_id=category_id)
+
+class AuthorDetailView(RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
