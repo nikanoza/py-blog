a168789bbb6639124653c3d2e31d03b2af60c208
Nika Nozadze <nikanozadze1010@gmail.com>
2023-09-19 12:03:12 +0400

    feat: create author model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    avatar = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
